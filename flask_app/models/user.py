eeae544e8ca53963d75923cddb1cf0d2aa6aa273
Catron-Gainey <catrong23@gmail.com>
2024-02-15 15:27:38 -0500

    added comments

--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -1,6 +1,6 @@
 # import the function that will return an instance of a connection
 from flask_app.config.mysqlconnection import connectToMySQL
-# model the class after the friend table from our database
+
 class Users:
     DB = "users_schema"
     def __init__( self , data ):
@@ -10,20 +10,20 @@ class Users:
         self.email = data['email']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
-    # Now we use class methods to query our database
+    # use class methods to query the database
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM users;"
         # make sure to call the connectToMySQL function with the schema you are targeting.
         results = connectToMySQL('users_schema').query_db(query)
-        # Create an empty list to append our instances of friends
+        # Create an empty list to append our instances
         users = []
-        # Iterate over the db results and create instances of friends with cls.
+        # Iterate over the db results and create instances with cls.
         for user in results:
             users.append(cls(user))
         return users
         
-    # the save method will be used when we need to save a new friend to our database
+    # the save method will be used when we need to save to our database
     @classmethod
     def save(cls, data):
         query = """INSERT INTO users (first_name, last_name, email)
